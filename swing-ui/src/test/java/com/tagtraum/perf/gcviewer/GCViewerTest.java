e28bf393878ae27e9dc88f906996a160b2e08251
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2021-03-02 17:30:58 +0100

    cleanup

--- swing-ui/src/test/java/com/tagtraum/perf/gcviewer/GCViewerTest.java
+++ swing-ui/src/test/java/com/tagtraum/perf/gcviewer/GCViewerTest.java
@@ -24,71 +24,73 @@ import com.tagtraum.perf.gcviewer.view.UnittestHelper;
  * @author martin.geldmacher
  */
 public class GCViewerTest {
-	
+
 	@Rule
-	public TemporaryFolder temporaryFolder = new TemporaryFolder(); 
+	public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
-    @Test
-    public void singleArgumentOpensGui() throws Exception {
-        GCViewerGuiController controller = mock(GCViewerGuiController.class);
-        GCViewer gcViewer = new GCViewer(controller, new GCViewerArgsParser());
+	@Test
+	public void singleArgumentOpensGui() throws Exception {
+		GCViewerGuiController controller = mock(GCViewerGuiController.class);
+		GCViewer gcViewer = new GCViewer(controller, new GCViewerArgsParser());
 
-        int exitValue = gcViewer.doMain("some_gc.log");
-        verify(controller).startGui(new GcResourceFile("some_gc.log"));
-        assertThat("exitValue of doMain", exitValue, is(0));
-    }
-
-    @Test
-    public void singleArgumentWithSeriesOpensGui() throws Exception {
-        GCViewerGuiController controller = mock(GCViewerGuiController.class);
-        GCViewer gcViewer = new GCViewer(controller, new GCViewerArgsParser());
-
-        int exitValue = gcViewer.doMain("some_gc.log.0;some_gc.log.1;some_gc.log.2");
-        verify(controller).startGui(new GcResourceSeries(Arrays.asList(new GcResourceFile("some_gc.log.0"), new GcResourceFile("some_gc.log.1"), new GcResourceFile("some_gc.log.2"))));
-        assertThat("result of doMain", exitValue, is(0));
-    }
-
-    @Test
-    public void moreThan3ArgumentsPrintsUsage() throws Exception {
-        GCViewerGuiController controller = mock(GCViewerGuiController.class);
-        GCViewer gcViewer = new GCViewer(controller, new GCViewerArgsParser());
-
-        int exitValue = gcViewer.doMain("argument1", "argument2", "argument3", "argument4");
-        verify(controller, never()).startGui(any(GCResource.class));
-        assertThat("result of doMain", exitValue, is(-3));
-    }
-
-    @Test
-    public void export() throws Exception {
-        GCViewerGuiController controller = mock(GCViewerGuiController.class);
-        GCViewer gcViewer = new GCViewer(controller, new GCViewerArgsParser());
-        
-        File in = new File(UnittestHelper.getResource("openjdk/SampleSun1_7_0-01_G1_young.txt").toURI());
-		int exitValue = gcViewer.doMain(filename(in), filename(temporaryFolder.newFile("export.csv")),
-				filename(temporaryFolder.newFile("export.png")), "-t", "PLAIN");
-        verify(controller, never()).startGui(any(GCResource.class));
-        assertThat("result of doMain", exitValue, is(0));
-    }
-
-	private String filename(File file) {
-		return file.getAbsoluteFile().toString();
+		int exitValue = gcViewer.doMain("some_gc.log");
+		verify(controller).startGui(new GcResourceFile("some_gc.log"));
+		assertThat("exitValue of doMain", exitValue, is(0));
 	}
 
-    @Test
-    public void exportFileNotFound() throws Exception {
-        GCViewerGuiController controller = mock(GCViewerGuiController.class);
-        GCViewer gcViewer = new GCViewer(controller, new GCViewerArgsParser());
+	@Test
+	public void singleArgumentWithSeriesOpensGui() throws Exception {
+		GCViewerGuiController controller = mock(GCViewerGuiController.class);
+		GCViewer gcViewer = new GCViewer(controller, new GCViewerArgsParser());
 
-        int exitValue = gcViewer.doMain("doesNotExist.log", "export.csv", "-t", "PLAIN");
-        verify(controller, never()).startGui(any(GCResource.class));
-        assertThat("result of doMain", exitValue, is(-1));
-    }
+		int exitValue = gcViewer.doMain("some_gc.log.0;some_gc.log.1;some_gc.log.2");
+		verify(controller).startGui(new GcResourceSeries(Arrays.asList(new GcResourceFile("some_gc.log.0"),
+				new GcResourceFile("some_gc.log.1"), new GcResourceFile("some_gc.log.2"))));
+		assertThat("result of doMain", exitValue, is(0));
+	}
 
-    @Test
-    public void illegalExportFormat() throws Exception {
-        GCViewer gcViewer = new GCViewer();
+	@Test
+	public void moreThan3ArgumentsPrintsUsage() throws Exception {
+		GCViewerGuiController controller = mock(GCViewerGuiController.class);
+		GCViewer gcViewer = new GCViewer(controller, new GCViewerArgsParser());
 
-        int exitValue = gcViewer.doMain("-t", "INVALID");
-        assertThat("result of doMain", exitValue, is(-2));
-    }
+		int exitValue = gcViewer.doMain("argument1", "argument2", "argument3", "argument4");
+		verify(controller, never()).startGui(any(GCResource.class));
+		assertThat("result of doMain", exitValue, is(-3));
+	}
+
+	@Test
+	public void export() throws Exception {
+		GCViewerGuiController controller = mock(GCViewerGuiController.class);
+		GCViewer gcViewer = new GCViewer(controller, new GCViewerArgsParser());
+
+		File in = new File(UnittestHelper.getResource("openjdk/SampleSun1_7_0-01_G1_young.txt").toURI());
+		int exitValue = gcViewer.doMain(filename(in), filename(temporaryFolder.newFile("export.csv")),
+				filename(temporaryFolder.newFile("export.png")), "-t", "PLAIN");
+		verify(controller, never()).startGui(any(GCResource.class));
+		assertThat("result of doMain", exitValue, is(0));
+	}
+
+	@Test
+	public void exportFileNotFound() throws Exception {
+		GCViewerGuiController controller = mock(GCViewerGuiController.class);
+		GCViewer gcViewer = new GCViewer(controller, new GCViewerArgsParser());
+
+		int exitValue = gcViewer.doMain("doesNotExist.log", "export.csv", "-t", "PLAIN");
+		verify(controller, never()).startGui(any(GCResource.class));
+		assertThat("result of doMain", exitValue, is(-1));
+	}
+
+	@Test
+	public void illegalExportFormat() throws Exception {
+		GCViewerGuiController controller = mock(GCViewerGuiController.class);
+		GCViewer gcViewer = new GCViewer(controller, new GCViewerArgsParser());
+
+		int exitValue = gcViewer.doMain("-t", "INVALID");
+		assertThat("result of doMain", exitValue, is(-2));
+	}
+
+	private static String filename(File file) {
+		return file.getAbsoluteFile().toString();
+	}
 }
